9a976d64ecbc07ba2ac12a84806ccececb0b72d7
subhambiswajit <digu35@gmail.com>
2018-07-09 12:23:04 +0530

    Adding sms read feature from inbox and sent items

--- app/src/main/java/com/radicals/four/gocommit/Scheduler.java
+++ app/src/main/java/com/radicals/four/gocommit/Scheduler.java
@@ -1,7 +1,11 @@
 package com.radicals.four.gocommit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class Scheduler extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class Scheduler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheduler);
+        Intent intent = getIntent();
+        TextView scheduler_text_view = findViewById(R.id.analyzing_textView);
+        String inbox_messages = intent.getExtras().getString(TextAnalyzerActivity.sms_inbox_key);
+        String sent_messages = intent.getExtras().getString(TextAnalyzerActivity.sms_sent_key);
+        scheduler_text_view.setText(inbox_messages + sent_messages);
+        scheduler_text_view.setMovementMethod(new ScrollingMovementMethod());
     }
 }
--- app/src/main/java/com/radicals/four/gocommit/TextAnalyzerActivity.java
+++ app/src/main/java/com/radicals/four/gocommit/TextAnalyzerActivity.java
@@ -1,6 +1,8 @@
 package com.radicals.four.gocommit;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class TextAnalyzerActivity extends AppCompatActivity {
-    public final static String analyze_string_key = "ANALYZE_KEY";
+    public final static String sms_inbox_key = "INBOX_KEY";
+    public final static String sms_sent_key = "SENT_KEY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +25,30 @@ public class TextAnalyzerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText analyzing_string = findViewById(R.id.analyzing_edit_text);
-                Intent analyze_to_scheduler = new Intent(TextAnalyzerActivity.this, Scheduler.class);
-                analyze_to_scheduler.putExtra(analyze_string_key, analyzing_string.getText());
-                startActivity(analyze_to_scheduler);
+                Intent analyze_to_scheduler_intent = new Intent(TextAnalyzerActivity.this, Scheduler.class);
+                analyze_to_scheduler_intent.putExtra(sms_inbox_key,grabInboxMessage(sms_type.INBOX));
+                analyze_to_scheduler_intent.putExtra(sms_sent_key,grabInboxMessage(sms_type.SENT));
+                startActivity(analyze_to_scheduler_intent);
             }
         });
+    }
+
+    public String grabInboxMessage(sms_type type) {
+        String sms_path = "content://sms/inbox";
+        if (type == sms_type.SENT)
+            sms_path = "content://sms/sent";
+        Uri uriSMSURI = Uri.parse(sms_path);
+        Cursor cur = getContentResolver().query(uriSMSURI, null, null, null,null);
+        String sms = "";
+        while(cur.moveToNext())
+        {
+            sms += "From :" + cur.getString(cur.getColumnIndex("address")) + " : " + cur.getString(cur.getColumnIndex("body"))+"\n";
+        }
+        return sms;
+    }
+
+    public enum sms_type {
+        INBOX,
+        SENT
     }
 }
